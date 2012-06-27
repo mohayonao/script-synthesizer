f8b9f8c025d3871168b424b839ef36939a310002
mohayonao <mohayonao@gmail.com>
2012-06-28 07:08:24 +0900

    update: preset 013 Marimba

--- presets/013.js
+++ presets/013.js
@@ -3,25 +3,19 @@
  * author: @mohayonao
  */
 function synthdef(freq) {
-    var op4 = T("*", T("oscx", T("phasor", freq * 2), 0.2),
-                     T("adsr", "24db", 100));
-    var op3 = T("*", T("oscx", T("+", T("phasor", freq), op4), 0.4),
-                     T("adsr", "24db", 100));
-    var op2 = T("*", T("oscx", T("phasor", freq * 2), 0.25).set({fb:0.15}),
-                     T("adsr", "24db", 250));
-    var op1 = T("*", T("oscx", T("+", T("phasor", freq), op2)),
-                     T("adsr", "24db", 5, 250));
-    var synth = T("+", op1, op3);
+    var op1 = T("oscx", T("phasor", freq * 2), 0.2);
+    var op2 = T("*", T("oscx", T("+", T("phasor", freq), op1), 0.05),
+                      T("adsr", "32db", 0, 200, 0.2, 100));
+    var op3 = T("oscx", T("phasor", freq * 8), 0.05);
+    var op4 = T("*", T("oscx", T("+", T("phasor", freq), op3), 0.1),
+                     T("adsr", "24db", 0, 300, 0.3, 200));
+    var synth = T("+", op2, op4);
     synth.keyon = function() {
-        op1.args[1].bang();
         op2.args[1].bang();
-        op3.args[1].bang();
         op4.args[1].bang();
     };
     synth.keyoff = function() {
-        op1.args[1].keyoff();
         op2.args[1].keyoff();
-        op3.args[1].keyoff();
         op4.args[1].keyoff();
     };
     return synth;
