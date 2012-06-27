1cd1ecd20a2caded15ff592194bf355f047ce749
mohayonao <mohayonao@gmail.com>
2012-06-27 11:23:23 +0900

    fix

--- presets/000.js
+++ presets/000.js
@@ -1,11 +1,13 @@
-var synth = T("sin");
+var synth = T("*", T("sin"),
+                   T("adsr", "24db", 5, 100, 0.7, 10));
 
 synth.noteon = function(midinote, velocity) {
-    synth.freq.value = timbre.utils.mtof(midinote);
-    synth.mul = velocity / 128;
+    synth.args[0].freq.value = timbre.utils.mtof(midinote);
+    synth.args[0].mul = velocity / 128;
+    synth.args[1].bang();
 };
 synth.noteoff = function(midinote) {
-    synth.mul = 0;
+    synth.args[1].keyOff();
 };
 
 return synth;
