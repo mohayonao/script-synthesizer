2c9baad010afca35507d9da5ca938c08d0856760
mohayonao <mohayonao@gmail.com>
2012-06-27 10:01:33 +0900

    add: names of General MIDI preset instruments

---
+++ public/javascripts/preset.js
@@ -0,0 +1,163 @@
+var Preset = [
+    "none",
+    
+    // (1..8) Piano
+    "Acoustic Piano (NOT IMPLEMENTED)",
+    "Bright Piano (NOT IMPLEMENTED)",
+    "Electric Grand Piano (NOT IMPLEMENTED)",
+    "Honky-tonk Piano (NOT IMPLEMENTED)",
+    "Electric Piano (NOT IMPLEMENTED)",
+    "Electric Piano 2 (NOT IMPLEMENTED)",
+    "Harpsichode (NOT IMPLEMENTED)",
+    "Clavi (NOT IMPLEMENTED)",
+
+    // (9..16) Chromatic Percussion
+    "Celesta (NOT IMPLEMENTED)",
+    "Glockenspiel (NOT IMPLEMENTED)",
+    "Music Box (NOT IMPLEMENTED)",
+    "Vibraphone (NOT IMPLEMENTED)",
+    "Marimba (NOT IMPLEMENTED)",
+    "Xylophone (NOT IMPLEMENTED)",
+    "TubularBell (NOT IMPLEMENTED)",
+    "Dulcimer (NOT IMPLEMENTED)",
+
+    // (17..24) Organ
+    "Drawbar Organ (NOT IMPLEMENTED)",
+    "Percussive Organ (NOT IMPLEMENTED)",
+    "Rock Organ (NOT IMPLEMENTED)",
+    "Church Organ (NOT IMPLEMENTED)",
+    "Reed Organ (NOT IMPLEMENTED)",
+    "Accordion (NOT IMPLEMENTED)",
+    "Harmonica (NOT IMPLEMENTED)",
+    "Tango Accordion (NOT IMPLEMENTED)",
+
+    // (25..32) Guitar
+    "Acoustic Guitar (nylon) (NOT IMPLEMENTED)",
+    "Acoustic Guitar (steel) (NOT IMPLEMENTED)",
+    "Electric Guitar (jazz) (NOT IMPLEMENTED)",
+    "Electric Guitar (clean) (NOT IMPLEMENTED)",
+    "Electric Guitar (muted) (NOT IMPLEMENTED)",
+    "Orverdriven Guitar (NOT IMPLEMENTED)",
+    "Distortion Guitar (NOT IMPLEMENTED)",
+    "Guitar Harmonics (NOT IMPLEMENTED)",
+
+    // (33..40) Bass
+    "Acoustic Bass (NOT IMPLEMENTED)",
+    "Electric Bass (finger) (NOT IMPLEMENTED)",
+    "Electric Bass (pick) (NOT IMPLEMENTED)",
+    "Fretless Bass (NOT IMPLEMENTED)",
+    "Slap Bass 1 (NOT IMPLEMENTED)",
+    "Slap Bass 2 (NOT IMPLEMENTED)",
+    "Synth Bass 2 (NOT IMPLEMENTED)",
+    "Synth Bass 2 (NOT IMPLEMENTED)",
+
+    // (41..48) Strings
+    "Violin (NOT IMPLEMENTED)",
+    "Viola (NOT IMPLEMENTED)",
+    "Cello (NOT IMPLEMENTED)",
+    "Double bass (NOT IMPLEMENTED)",
+    "Tremolo Strings (NOT IMPLEMENTED)",
+    "Pizzicato Strings (NOT IMPLEMENTED)",
+    "Orchestral Harp (NOT IMPLEMENTED)",
+    "Timpani (NOT IMPLEMENTED)",
+
+    // (49..56) Ensemble
+    "String Ensemble 1 (NOT IMPLEMENTED)",
+    "String Ensemble 2 (NOT IMPLEMENTED)",
+    "Synth String 1 (NOT IMPLEMENTED)",
+    "Synth String 2 (NOT IMPLEMENTED)",
+    "Voice Aahs (NOT IMPLEMENTED)",
+    "Voice Oohs (NOT IMPLEMENTED)",
+    "Synth Voice (NOT IMPLEMENTED)",
+    "Orchestra Hit (NOT IMPLEMENTED)",
+
+    // (57..64) Brass
+    "Trumpet (NOT IMPLEMENTED)",
+    "Trombone (NOT IMPLEMENTED)",
+    "Tuba (NOT IMPLEMENTED)",
+    "Muted Trumpet (NOT IMPLEMENTED)",
+    "French horn (NOT IMPLEMENTED)",
+    "Brass Section (NOT IMPLEMENTED)",
+    "Synth Brass 1 (NOT IMPLEMENTED)",
+    "Synth Brass 2 (NOT IMPLEMENTED)",
+
+    // (65..72) Reed
+    "Soprano Sax (NOT IMPLEMENTED)",
+    "Alto Sax (NOT IMPLEMENTED)",
+    "Tenor Sax (NOT IMPLEMENTED)",
+    "Baritone Sax (NOT IMPLEMENTED)",
+    "Oboe (NOT IMPLEMENTED)",
+    "English Horn (NOT IMPLEMENTED)",
+    "Bassoon (NOT IMPLEMENTED)",
+    "Clarinet (NOT IMPLEMENTED)",
+
+    // (73..80) Pipe
+    "Piccolo (NOT IMPLEMENTED)",
+    "Flute (NOT IMPLEMENTED)",
+    "Recorder (NOT IMPLEMENTED)",
+    "Pan Flute (NOT IMPLEMENTED)",
+    "Blown Bottle (NOT IMPLEMENTED)",
+    "Shakuhachi (NOT IMPLEMENTED)",
+    "Whistle (NOT IMPLEMENTED)",
+    "Ocarina (NOT IMPLEMENTED)",
+
+    // (81..88) Synth Lead
+    "Lead 1 (square) (NOT IMPLEMENTED)",
+    "Lead 2 (sawtooth) (NOT IMPLEMENTED)",
+    "Lead 3 (callipoe) (NOT IMPLEMENTED)",
+    "Lead 4 (chiff) (NOT IMPLEMENTED)",
+    "Lead 5 (charang) (NOT IMPLEMENTED)",
+    "Lead 6 (voice) (NOT IMPLEMENTED)",
+    "Lead 7 (fifths) (NOT IMPLEMENTED)",
+    "Lead 8 (bass + lead) (NOT IMPLEMENTED)",
+    
+    // (89..96) Synth Pad
+    "Pad 1 (Fantasia) (NOT IMPLEMENTED)",
+    "Pad 2 (warm) (NOT IMPLEMENTED)",
+    "Pad 3 (polysynth) (NOT IMPLEMENTED)",
+    "Pad 4 (choir) (NOT IMPLEMENTED)",
+    "Pad 5 (bowed) (NOT IMPLEMENTED)",
+    "Pad 6 (metallic) (NOT IMPLEMENTED)",
+    "Pad 7 (halo) (NOT IMPLEMENTED)",
+    "Pad 8 (sweep) (NOT IMPLEMENTED)",
+    
+    // (97..104) Synth Effects
+    "FX 1 (rain) (NOT IMPLEMENTED)",
+    "FX 2 (soundtrack) (NOT IMPLEMENTED)",
+    "FX 3 (crystal) (NOT IMPLEMENTED)",
+    "FX 4 (atmosphere) (NOT IMPLEMENTED)",
+    "FX 5 (brightness) (NOT IMPLEMENTED)",
+    "FX 6 (goblins) (NOT IMPLEMENTED)",
+    "FX 7 (echoes) (NOT IMPLEMENTED)",
+    "FX 8 (sci-fi) (NOT IMPLEMENTED)",
+    
+    // (105..112) Ethnic
+    "Sitar (NOT IMPLEMENTED)",
+    "Banjo (NOT IMPLEMENTED)",
+    "Shamisen (NOT IMPLEMENTED)",
+    "Koto (NOT IMPLEMENTED)",
+    "Kalimba (NOT IMPLEMENTED)",
+    "Bagpipe (NOT IMPLEMENTED)",
+    "Fiddle (NOT IMPLEMENTED)",
+    "Shanai (NOT IMPLEMENTED)",
+    
+    // (113..120) Percussive
+    "Tinkle Bell (NOT IMPLEMENTED)",
+    "Agogo (NOT IMPLEMENTED)",
+    "Steel Drums (NOT IMPLEMENTED)",
+    "Woodblock (NOT IMPLEMENTED)",
+    "Taiko Drum (NOT IMPLEMENTED)",
+    "Melodic Tom (NOT IMPLEMENTED)",
+    "Synth Drum (NOT IMPLEMENTED)",
+    "Reverse Cymbal (NOT IMPLEMENTED)",
+    
+    // (121..128) Sound effects
+    "Guitar Fret Noise (NOT IMPLEMENTED)",
+    "Breath Noise (NOT IMPLEMENTED)",
+    "Seashore (NOT IMPLEMENTED)",
+    "Bird Tweet (NOT IMPLEMENTED)",
+    "Telephone Ring (NOT IMPLEMENTED)",
+    "Helicopter (NOT IMPLEMENTED)",
+    "Applause (NOT IMPLEMENTED)",
+    "Gunshot (NOT IMPLEMENTED)",
+];
