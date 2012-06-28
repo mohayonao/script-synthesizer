9d7bacf91b98f1c4ade732d009d3541ac4adbdf4
mohayonao <mohayonao@gmail.com>
2012-06-28 09:40:17 +0900

    update: preset 009 Celesta

--- presets/009.js
+++ presets/009.js
@@ -4,10 +4,10 @@
  */
 function synthdef(freq) {
     var op1 = T("oscx", T("phasor", 200), 0.01).set({fb:0.1});
-    var op2 = T("*", T("oscx", T("+", T("phasor", freq), op1), 0.2),
+    var op2 = T("*", T("oscx", T("+", T("phasor", freq), op1), 0.4),
                      T("adsr", "32db", 0, 450, 0.4, 500));
     var op3 = T("oscx", T("phasor", freq * 14), 0.1);
-    var op4 = T("*", T("oscx", T("+", T("phasor", freq * 2), op3), 0.5),
+    var op4 = T("*", T("oscx", T("+", T("phasor", freq * 2), op3), 1.0),
                      T("adsr", "24db", 0, 250, 0.1, 500));
     var synth = T("+", op2, op4);
     synth.keyon = function() {
