a32239cfeafdcd7d4b3f6928caf46d194317d544
mohayonao <mohayonao@gmail.com>
2012-06-28 09:40:03 +0900

    update: preset 013 Marimba

--- presets/013.js
+++ presets/013.js
@@ -4,10 +4,10 @@
  */
 function synthdef(freq) {
     var op1 = T("oscx", T("phasor", freq * 2), 0.2);
-    var op2 = T("*", T("oscx", T("+", T("phasor", freq), op1), 0.05),
+    var op2 = T("*", T("oscx", T("+", T("phasor", freq), op1), 0.5),
                       T("adsr", "32db", 0, 200, 0.2, 100));
     var op3 = T("oscx", T("phasor", freq * 8), 0.05);
-    var op4 = T("*", T("oscx", T("+", T("phasor", freq), op3), 0.1),
+    var op4 = T("*", T("oscx", T("+", T("phasor", freq), op3)),
                      T("adsr", "24db", 0, 300, 0.3, 200));
     var synth = T("+", op2, op4);
     synth.keyon = function() {
