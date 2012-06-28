9b9380f6193c6e3071e0473eb3afcc943355e2ea
mohayonao <mohayonao@gmail.com>
2012-06-28 09:13:28 +0900

    add: UI, Keyboard to Keyboard

--- public/javascripts/main.coffee
+++ public/javascripts/main.coffee
@@ -14,6 +14,7 @@ jQuery ->
             finally
                 _synthdef = null unless _synthdef instanceof Function
             if _synthdef
+                receiver.onAllSoundOff()
                 window.synthdef = _synthdef
                 $("#code").css "color", "black"
             else
@@ -32,6 +33,10 @@ jQuery ->
     id = location.search.substr 1
     if /^\d{1,3}$/.test id then selector.val id|0
     selector.change()
+
+    isEditing = false
+    $("#code").on "focus", (e)-> isEditing = true
+    $("#code").on "blur" , (e)-> isEditing = false
 
     $("#code").on "keydown", (e)->
         if e.ctrlKey and e.keyCode is 32
@@ -66,7 +71,7 @@ jQuery ->
     window.testplay = (b)->
         if b then interval.on() else interval.off()
 
-    # synth
+    # Synth
     synth = T("+").play()
     synth.noteOn = {}
 
@@ -91,3 +96,57 @@ jQuery ->
     receiver.onAllSoundOff = ()->
         while synth.args.length > 0
             delete synth.noteOn[synth.args.shift().notenumber]
+
+
+    # Keyboad to Keyboad
+    key2notenumber = {
+        90 : 48 # Z -> C2
+        83 : 49 # S -> C+2
+        88 : 50 # X -> D2
+        68 : 51 # D -> D+2
+        67 : 52 # C -> E2
+        86 : 53 # V -> F2
+        71 : 54 # G -> F+2
+        66 : 55 # B -> G2
+        72 : 56 # H -> G+2
+        78 : 57 # N -> A2
+        74 : 58 # J -> A+2
+        77 : 59 # M -> B2
+        188: 60 # , -> C3
+        76 : 61 # L -> C+3
+        190: 62 # . -> D3
+        186: 63 # ; -> D+3
+
+        81 : 60 # Q -> C3
+        50 : 61 # 2 -> C+3
+        87 : 62 # W -> D3
+        51 : 63 # 3 -> D+3
+        69 : 64 # E -> E3
+        82 : 65 # R -> F3
+        53 : 66 # 5 -> F+3
+        84 : 67 # T -> G3
+        54 : 68 # 6 -> G+3
+        89 : 69 # Y -> A3
+        55 : 70 # 7 -> A+3
+        85 : 71 # U -> B3
+        73 : 72 # I -> C4
+        57 : 73 # 9 -> C#4
+        79 : 74 # O -> D4
+        48 : 75 # 0 -> D+4
+        80 : 76 # P -> E4
+    }
+
+    isKeyDown = {}
+    window.addEventListener "keydown", (e)->
+        return if isEditing
+        return if isKeyDown[e.keyCode]
+        notenumber = key2notenumber[e.keyCode]
+        if notenumber
+            receiver.onNoteOn notenumber, 64
+        isKeyDown[e.keyCode] = true
+
+    window.addEventListener "keyup", (e)->
+        return if isEditing
+        notenumber = key2notenumber[e.keyCode]
+        if notenumber then receiver.onNoteOff notenumber, 0
+        isKeyDown[e.keyCode] = false
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@
   "use strict";
 
   jQuery(function() {
-    var eval_synth, id, interval, prev_code, receiver, selector, synth;
+    var eval_synth, id, interval, isEditing, isKeyDown, key2notenumber, prev_code, receiver, selector, synth;
     window.synthdef = null;
     prev_code = "";
     eval_synth = function(code) {
@@ -21,6 +21,7 @@
           }
         }
         if (_synthdef) {
+          receiver.onAllSoundOff();
           window.synthdef = _synthdef;
           return $("#code").css("color", "black");
         } else {
@@ -46,6 +47,13 @@
       selector.val(id | 0);
     }
     selector.change();
+    isEditing = false;
+    $("#code").on("focus", function(e) {
+      return isEditing = true;
+    });
+    $("#code").on("blur", function(e) {
+      return isEditing = false;
+    });
     $("#code").on("keydown", function(e) {
       if (e.ctrlKey && e.keyCode === 32) {
         return e.preventDefault();
@@ -118,7 +126,7 @@
       var _ref;
       return (_ref = synth.noteOn[notenumber]) != null ? _ref.keyoff() : void 0;
     };
-    return receiver.onAllSoundOff = function() {
+    receiver.onAllSoundOff = function() {
       var _results;
       _results = [];
       while (synth.args.length > 0) {
@@ -126,6 +134,67 @@
       }
       return _results;
     };
+    key2notenumber = {
+      90: 48,
+      83: 49,
+      88: 50,
+      68: 51,
+      67: 52,
+      86: 53,
+      71: 54,
+      66: 55,
+      72: 56,
+      78: 57,
+      74: 58,
+      77: 59,
+      188: 60,
+      76: 61,
+      190: 62,
+      186: 63,
+      81: 60,
+      50: 61,
+      87: 62,
+      51: 63,
+      69: 64,
+      82: 65,
+      53: 66,
+      84: 67,
+      54: 68,
+      89: 69,
+      55: 70,
+      85: 71,
+      73: 72,
+      57: 73,
+      79: 74,
+      48: 75,
+      80: 76
+    };
+    isKeyDown = {};
+    window.addEventListener("keydown", function(e) {
+      var notenumber;
+      if (isEditing) {
+        return;
+      }
+      if (isKeyDown[e.keyCode]) {
+        return;
+      }
+      notenumber = key2notenumber[e.keyCode];
+      if (notenumber) {
+        receiver.onNoteOn(notenumber, 64);
+      }
+      return isKeyDown[e.keyCode] = true;
+    });
+    return window.addEventListener("keyup", function(e) {
+      var notenumber;
+      if (isEditing) {
+        return;
+      }
+      notenumber = key2notenumber[e.keyCode];
+      if (notenumber) {
+        receiver.onNoteOff(notenumber, 0);
+      }
+      return isKeyDown[e.keyCode] = false;
+    });
   });
 
 }).call(this);
