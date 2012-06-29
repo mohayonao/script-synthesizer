ad54c0c2fc53da1c895fa183873d7851d4dc4f1a
mohayonao <mohayonao@gmail.com>
2012-06-29 20:37:03 +0900

    update: preset 082 Lead 2 (sawtooth)

--- presets/082.js
+++ presets/082.js
@@ -2,7 +2,7 @@
  * 082: Lead 2 (sawtooth)
  * author: @mohayonao
  */
-function synthdef(freq) {
+function synthdef(freq, notenumber, velocity) {
     var synth = T("*", T("saw", freq),
                        T("adsr", "24db", 5, 100, 0.7, 10));
     synth.keyon = function() {
